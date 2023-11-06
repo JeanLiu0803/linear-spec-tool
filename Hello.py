51fbb015e1ff40666499d520e73b604b17f1cde9
Jean Liu <joan80987@gmail.com>
2023-11-07 00:39:46 +0800

    update

--- Hello.py
+++ Hello.py
@@ -32,10 +32,9 @@ def run():
         st.write("### 2. Please input x and y value for each point")
         st.number_input("X value (Frequency)", key = "x_value", step = 0.01)
         st.number_input("Y value (Mag.)", key = "y_value", step = 0.01)
-        st.button("Reset", key = "reset")
 
         point_df = pd.DataFrame(columns = ["x_value", "y_value"])
-        if st.button("Add point", key = "add_point"):
+        if st.button("Add point", key = "add_point_but"):
             point_df = point_df.append({"x_value": st.session_state.x_value, "y_value": st.session_state.y_value}, ignore_index = True)
             st.write(point_df)
         if st.button("Delete point", key = "delete_point"):
